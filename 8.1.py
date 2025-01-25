51432a8e0a4fba79a9a023d637140888b5bb77f3
genn125 <genn125@gmail.com>
2025-01-25 20:25:29 +0300

    Try и Except

--- 8.1.py
+++ 8.1.py
@@ -3,10 +3,14 @@
 
 def add_everything_up (a, b):   # может быть как числа(int, float), так и строки(str)
     try:
+
         # i=a+b # почему много после запятой?
-       i = round((a + b), 3)
-    except TypeError:
+        i = round((a + b), 3)
+        print('_________________Normal no')
+    except TypeError as ext:
+        print(f'_________________ {ext}, возвращаем строковое представление данных ')
         i = str(a) + str(b)
+
     return i
 
 print(add_everything_up(123.456, 'строка'))
@@ -14,3 +18,4 @@ print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
 
 print(add_everything_up('ОбеДве ', 'строки'))
+print(add_everything_up(123.456789, 76543.21))
