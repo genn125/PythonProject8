4f58f35a2ca7e4bcbc41c2f6c492a83f654e3f28
genn125 <genn125@gmail.com>
2025-01-29 15:26:12 +0300

    Сложные моменты и исключения в стеке вызовов функции

--- 8.2.py
+++ 8.2.py
@@ -1,21 +1,27 @@
-# 8.2 Сложные моменты и исключения в стеке вызовов функции
-# Задача "План перехват"
+#8.2 Сложные моменты и исключения в стеке вызовов функции
+# Задача "План перехват":
 
-def personal_sum(*numbers):
+def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for i in numbers:
-        for j in i:
-            try:
-                result += j
-            except TypeError:
-                incorrect_data += 1
-                print(f'некорректный тип данных для подсчета суммы - {j}')
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'{incorrect_data} - Некорректный тип данных для подсчёта суммы: {i}')
     return result, incorrect_data
 
-
-def calculate_average(numbers)
-
+def calculate_average(numbers):
+    if isinstance(numbers, int):
+        print(f'В numbers записан некорректный тип данных.')
+    try:
+        personal_sum_1 = personal_sum(numbers)
+        return personal_sum_1[0] / (len(numbers) - personal_sum_1[1])
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        return None
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
